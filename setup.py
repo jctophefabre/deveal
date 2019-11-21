83e2b3dc38835939e5e10994bf7f4578060fec9f
Jean-Christophe Fabre <jean-christophe.fabre@inra.fr>
2019-11-21 13:01:35 +0100

    Update of main template

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name = 'deveal',
-      version = '1.0.0',
+      version = '1.0.1',
       description = 'A reveal.js helper tool',
       long_description=readme(),
       author='Jean-Christophe Fabre',
